c9d74dcf38c42ee95bff073c230ff66b64ddde50
maudnals <maud.nalpas@gmail.com>
2025-04-25 11:38:20 +0200

    Refactor: Created separate compnent for labelled field

---
+++ src/components/LabelledFieldComponent.tsx
@@ -0,0 +1,18 @@
+import "./LabelledFieldComponent.css";
+
+interface LabelledFieldComponentProps {
+  label: string;
+  value: string;
+}
+
+export default function LabelledFieldComponent({
+  label,
+  value,
+}: LabelledFieldComponentProps) {
+  return (
+    <div className="labelled-field">
+      <div className="label">{label}</div>
+      <div>{value}</div>
+    </div>
+  );
+}
