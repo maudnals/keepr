5e49f4557862f6934cce341d64b333821f131981
maudnals <maud.nalpas@gmail.com>
2025-05-15 05:56:02 +0200

    Feat: Simplified component

--- src/components/EditableDateComponent.tsx
+++ src/components/EditableDateComponent.tsx
@@ -1,15 +1,11 @@
-import Check from "@mui/icons-material/Check";
-import Close from "@mui/icons-material/Close";
-import Edit from "@mui/icons-material/Edit";
-import IconButton from "@mui/material/IconButton";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 
 interface EditableDateComponentProps {
-  dateValue: Dayjs;
-  setDate: Function;
+  dateValue: Dayjs | null;
+  setDate: (date: Dayjs | null) => void;
 }
 
 export default function EditableDateComponent({
@@ -17,17 +13,14 @@ export default function EditableDateComponent({
   setDate,
 }: EditableDateComponentProps) {
   return (
-    <>
-      {/* <div>{lastCheckin ? lastCheckin.toDateString() : "N/A"}</div> */}
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          value={dateValue}
-          onChange={(newValue) => {
-            setDate(dayjs(newValue));
-          }}
-          disableFuture
-        />
-      </LocalizationProvider>
-    </>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        value={dateValue}
+        onChange={(newValue) => {
+          setDate(dayjs(newValue));
+        }}
+        disableFuture
+      />
+    </LocalizationProvider>
   );
 }
