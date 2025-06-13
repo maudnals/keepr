c41d52c8f480c30bdc8d17ef75896a9d842192ae
maudnals <maud.nalpas@gmail.com>
2025-06-13 03:44:17 +0200

    feat: simplified button UI

--- src/components/SigninMenu.tsx
+++ src/components/SigninMenu.tsx
@@ -1,8 +1,6 @@
-import * as React from "react";
 import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import "./SigninMenu.css";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 interface SigninMenuProps {
   signout: () => void;
@@ -13,43 +11,12 @@ export default function SigninMenu({
   signout,
   authenticateAndSetData,
 }: SigninMenuProps) {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  function handleSignout() {
-    signout();
-    handleClose();
-  }
-
-  function handleRefresh() {
-    authenticateAndSetData();
-    handleClose();
-  }
-
   return (
     <>
-      <Button
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      >
-        Menu
-      </Button>
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        <MenuItem onClick={handleRefresh} className="auth-button">
-          <Button>Refresh</Button>
-        </MenuItem>
-        <MenuItem onClick={handleSignout}>
-          <Button>Sign out</Button>
-        </MenuItem>
-      </Menu>
+      <ButtonGroup size="small" aria-label="Refresh and Sign out">
+        <Button onClick={authenticateAndSetData}>Refresh</Button>
+        <Button onClick={signout}>Sign out</Button>
+      </ButtonGroup>
     </>
   );
 }
