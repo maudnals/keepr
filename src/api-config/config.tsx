0de7aa2e5f39ff399d9826f8c407db189510ec65
maudnals <maud.nalpas@gmail.com>
2025-06-13 03:35:11 +0200

    fix: build error fixes (temporary)

--- src/api-config/config.tsx
+++ src/api-config/config.tsx
@@ -1,3 +1,13 @@
+// Extend the Window interface to include custom properties
+declare global {
+  interface Window {
+    DISCOVERY_DOC: string;
+    SCOPES: string;
+    CLIENT_ID: string;
+    API_KEY: string;
+  }
+}
+
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC =
   "https://www.googleapis.com/discovery/v1/apis/people/v1/rest";
@@ -5,3 +15,6 @@ const DISCOVERY_DOC =
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
 const SCOPES = "https://www.googleapis.com/auth/contacts";
+
+window.DISCOVERY_DOC = DISCOVERY_DOC;
+window.SCOPES = SCOPES;
